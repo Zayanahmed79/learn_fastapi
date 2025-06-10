5480dc8747ea9b2e468a9e0514b65799fde6a422
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-10 16:29:13 +0500

    FastAPI for Patients added

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI ,  Path
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open("patients.json","r") as f:
+        data = json.load(f)
+    return data
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+    
+
+
+@app.get("/")
+def hello():
+    return {'message':'Hello World!'}
+
+@app.get("/about")
+def about():
+    return {
+            "message": "Zayan is a millionaire!"
+    }
+
+
+
+@app.get("/patient/{patient_id}")
+def view_patients(patient_id : str  = Path(..., description="ID of patients in DB.", example="P001")):
+    data = load_data() 
+
+    if patient_id in data:
+        return data[patient_id]
+    return {"error": "patient not found!"}
