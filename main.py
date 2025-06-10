e571e7f8453b05e6b70db10b55dfc98c2eb01f78
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-10 17:48:36 +0500

    sorted api updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI ,  Path , HTTPException
+from fastapi import FastAPI ,  Path , HTTPException ,Query
 import json
 
 
@@ -39,5 +39,26 @@ def view_patients(patient_id : str  = Path(..., description="ID of patients in D
     raise HTTPException(status_code=404 , detail= 'Patient not found!')
 
 
+
+
+
+@app.get("/sort")
+def sort_patients(sort_by : str = Query(..., description=f"Sort on the basis of height, weight or bmi "), order: str = Query('asc', description="sort in asc and desc order")):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"Invalid fields select from {valid_fields}")
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Invalid field select from asc and desc")
+    
+    data = load_data()
+
+    sort_data = True if order == "desc" else False 
+
+    sorted_data = sorted(data.values(), key=lambda x : x.get(sort_by, 0), reverse=sort_data)
+
+    return sorted_data
 
 
